fbcceb8e69d33eb3ea2a6b613762c378af08e962
David Cornu <davidjcornu@gmail.com>
2013-11-11 05:48:22 +0000

    Improve tests

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ require('./');
 
 var tmpdir    = path.join(__dirname, 'tmp', 'nodemailer');
 var transport = nodemailer.createTransport('MailPreview', {
-  dir: tmpdir,
+  dir:     tmpdir,
   browser: false
 });
 
@@ -36,10 +36,49 @@ var email = {
 
 describe('MailPreview', function(){
 
-  it('should add itself to nodemailer\'s transports', function(){
+  it('adds itself to nodemailer\'s transports', function(){
     assert(nodemailer.Transport.transports.MAILPREVIEW);
   });
 
-  // TODO
+  describe('.sendMail()', function(){
+    var error, response;
 
+    before(function(done){
+      transport.sendMail(email, function(e, r){
+        error    = e;
+        response = r;
+        done();
+      });
+    });
+
+    after(function(){
+      try {
+        // Cleanup files
+        fs.unlinkSync(response.html);
+        fs.unlinkSync(response.text);
+        fs.rmdirSync(path.dirname(response.html));
+      } catch (e) {
+        console.error('Could not clean up temporary files');
+        console.error(e.stack || e);
+      }
+    });
+
+    it('does not return an error', function(){
+      assert(error == null);
+    });
+
+    it('returns the details in the response', function(){
+      assert(response.html && response.text);
+    });
+
+    it('writes the text part of the email to the corresponding file', function(){
+      var body = fs.readFileSync(response.text, 'utf-8');
+      assert(body.indexOf("Hey jude, don't make it bad.") >= 0);
+    });
+
+    it('writes the html part of the email to the corresponding file', function(){
+      var body = fs.readFileSync(response.html, 'utf-8');
+      assert(body.indexOf("<h1>Hey Jude</h1>") >= 0);
+    });
+  });
 });
